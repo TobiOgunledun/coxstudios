e93ed61db014397738e15fd0702920c24324eb64
Tobi <87040784+TobiOgunledun@users.noreply.github.com>
2022-06-03 08:34:31 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,84 @@
+
+
+var menuOpen = document.getElementById("open");
+var menuClose = document.getElementById("close");
+var goBack = document.getElementById("back");
+var navBar = document.getElementById("nav-menu");
+var button = document.getElementById("button");
+var button2 = document.getElementById("button2");
+var content = document.getElementById("content");
+var content2 = document.getElementById("content2");
+var returnText = document.getElementById("content_text");
+var returnImg = document.getElementById("content_img");
+
+
+function showMenu(){
+    // console.log("i work");
+    navBar.style.right = 0;
+    menuOpen.style.display = "none";
+}
+function hideMenu(){
+    navBar.style.right = "";
+    menuOpen.style.display = "";
+}
+content2.style.display="none"
+function changeSlide(){
+    content.style.display="none";
+    content2.style.display="flex";
+    button2.style.display="block";
+    button.style.display="none";
+}
+function changeSlide2(){
+    content2.style.display="none"
+    content.style.display="flex";
+    returnText.style.animationDelay = ".15s";
+    returnImg.style.animationDelay= ".03s";
+    button.style.display="block";
+    button2.style.display="none";
+
+}
+
+
+firstSlide = document.getElementsByClassName("image");
+console.log(firstSlide.length);
+
+function next(){
+    firstSlide.style.transition = "margin .6s ease-in-out";
+    firstSlide.style.marginLeft = "-30vw";
+}
+function prev(){
+    firstSlide.style.transition = "margin .6s ease-in-out";
+    firstSlide.style.marginLeft = "0";
+}
+
+function next(direction){
+    let slide = document.getElementById("container");
+    scrollComplete = 0;
+    let slideBar = setInterval(function(){
+        if (direction == "left"){
+            slide.scrollLeft -= 300;
+        } else{
+            slide.scrollLeft += 300;
+        }
+        scrollComplete += 100;
+        if(scrollComplete >= 100){
+            window.clearInterval(slideBar);
+        }
+    }, 100);
+}
+
+function prev(direction){
+    let slide = document.getElementById("container");
+    scrollComplete = 0;
+    let slideBar = setInterval(function(){
+        if (direction == "left"){
+            slide.scrollLeft += 300;
+        } else{
+            slide.scrollLeft -= 300;
+        }
+        scrollComplete -= 100;
+        if(scrollComplete <= 100){
+            window.clearInterval(slideBar);
+        }
+    }, 100);
+}
